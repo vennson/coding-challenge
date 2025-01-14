ac049a9d3eee4a1cef582de52a1c6ce341c01cb9
vennson <bmiakoun@gmail.com>
2025-01-14 22:40:52 +0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { Button, Flex, Image, Stack } from '@mantine/core'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [images, setImages] = useState<string[]>([])
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+  function getImage(id: string) {
+    return `https://picsum.photos/seed/${id}/100`
+  }
+
+  function onAddImage() {
+    const date = new Date()
+    const image = getImage(date.toISOString())
+    console.log('@@ image', image)
+    setImages((prev) => [...prev, image])
+  }
+
+  function onRemoveRandom() {
+    const randomIndex = Math.floor(Math.random() * images.length)
+    const filtered = images.filter((_, index) => index !== randomIndex)
+    setImages(filtered)
+  }
+
+  return (
+    <div>
+      <main>
+        <Flex justify='center'>
+          <Flex direction='column' align='center' p='sm' w={320}>
+            <Stack gap='sm'>
+              {images.map((item) => (
+                <Image key={item} src={item} alt={item} />
+              ))}
+            </Stack>
+            <Flex direction='column' align='center' gap='xs' mt='lg'>
+              <Button onClick={onAddImage}>Add image</Button>
+              <Button onClick={onRemoveRandom} color='red'>
+                Remove random image
+              </Button>
+            </Flex>
+          </Flex>
+        </Flex>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
